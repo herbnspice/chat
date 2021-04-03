92bbbfd878ceb11f11a6391e18925c03b790c33c
Herbert Saladar <hrbrtsldr@gmail.com>
2021-04-03 21:46:33 +0800

    Form switching

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <Home msg="Welcome to Your Vue.js App"/>
-  <SignupForm />
-  <LoginForm />
+  <div v-if="showLogin">
+      <LoginForm />
+    <p> No account yet?  <span @click="showLogin=false"> Signup  </span >  instead </p>
+  </div>
+  <div v-else>
+      <SignupForm />
+    <p> Already registered? <span @click="showLogin=true"> Login  </span >  instead </p>
+
+  </div>
 </template>
 
 <script>
 import Home from './components/Home.vue'
 import SignupForm from './components/SignupForm.vue'
-import LoginForm from './components/SignupForm.vue'
+import LoginForm from './components/LoginForm.vue'
 
 export default {
   name: 'App',
@@ -16,6 +23,10 @@ export default {
     Home,
     SignupForm,
     LoginForm
+  },
+  setup(){
+    const showLogin = ref( true )
+    return{ showLogin }
   }
 }
 </script>
